0f6ea9019ca2c6a667dbf942869085908f009d11
Sooraj <sooraj.crafts@gmail.com>
2026-02-12 22:19:03 +0800

    Update lexa.rb

--- lexa.rb
+++ lexa.rb
@@ -1,12 +1,12 @@
 class Lexa < Formula
   desc "Grammar and style checker TUI"
   homepage "https://github.com/OWNER/lexa"
-  version "0.4.1"
+  version "0.4.2"
 
   on_macos do
     on_arm do
       url "https://github.com/suryanox/lexa/releases/download/v#{version}/lexa-darwin-arm64"
-      sha256 "410fcf745f62a69087068ac2d7805e61fe981b3b46297322f9707e4768e1001a"
+      sha256 "217d9d4adacbcc505bc1f3fbf042a6ce2122a7b2bc4b03fedc4eb5bd77691db4"
     end
   end
 
