fd5f22a2136ffcfcdae91360352a5b66e4f75635
Sooraj <sooraj.crafts@gmail.com>
2026-02-17 17:56:03 +0800

    Update crux.rb

--- crux.rb
+++ crux.rb
@@ -1,23 +1,23 @@
 class Crux < Formula
   desc "Terminal-first DB client: connect, query, and explore your data, fast"
   homepage "https://github.com/suryanox/crux"
-  version "0.1.1"
+  version "0.1.2"
   license "MIT"
 
   on_macos do
     on_intel do
       url "https://github.com/suryanox/crux/releases/download/v#{version}/crux-x86_64-apple-darwin.tar.gz"
-      sha256 "119cc41f02da2e056002c8227a58db84c048a5d0bc9a824291306633a442b369"
+      sha256 "15af0b62f94672fc182c697355f822a91fe082f8c0584ce33ff62e83732e0080"
     end
     on_arm do
       url "https://github.com/suryanox/crux/releases/download/v#{version}/crux-aarch64-apple-darwin.tar.gz"
-      sha256 "e5066e96260025ee913342f56aa7f7b96017c9fe039011ce8d3b7e2f8b9b3234"
+      sha256 "1616b2f097c1531db4b21df336274e5f137d2368c798d62895ab2e8c82e6c08a"
     end
   end
 
   on_linux do
     url "https://github.com/suryanox/crux/releases/download/v#{version}/crux-x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "aab44f06d429d700825e974c429c05350860950cd9a82c747e7d5dfc061718b5"
+    sha256 "d0c7b38d3a31441e9aafd819d48171d7a051d6a67d0502896fb91e57d8a25942"
   end
 
   def install
