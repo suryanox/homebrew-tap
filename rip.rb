5b8d8d507248790cfb32e5bebec931bb2a05e28e
Sooraj <sooraj.crafts@gmail.com>
2026-03-24 22:47:58 +0800

    update rip homebred

--- rip.rb
+++ rip.rb
@@ -1,18 +1,18 @@
 class Rip < Formula
   desc "A TUI to list processes bound to ports and kill them interactively"
   homepage "https://github.com/suryanox/rip"
-  version "0.2.0"
+  version "0.3.0"
   license "MIT"
 
   on_macos do
     on_intel do
       url "https://github.com/suryanox/rip/releases/download/v#{version}/rip-x86_64-apple-darwin.tar.gz"
-      # sha256 "3c8ddf1c3421df1fe2a9f80fd2bd5da9d5c5182c050094fedcba3fe15f35a51e"
+      # sha256 "99bea0226e03d197b3a4e975ded869eb63bb0776781d188b29640876956cd0ad"
     end
 
     on_arm do
       url "https://github.com/suryanox/rip/releases/download/v#{version}/rip-aarch64-apple-darwin.tar.gz"
-      # sha256 "560cd21d2d0ac1521f9474c4d3ef3995810e156a1fcb1d1fa930efa7b404c2ff"
+      # sha256 "60304f83cab7e85f73c4cbf4bf63e1aec2993290a97a93d1c6a38e11bc5786a9"
     end
   end
 
