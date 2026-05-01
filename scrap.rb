dee79f535e75e16e29ed024552f76a2770507025
Sooraj <sooraj.crafts@gmail.com>
2026-05-01 22:51:31 +0800

    Update scrap.rb

--- scrap.rb
+++ scrap.rb
@@ -1,18 +1,18 @@
 class Scrap < Formula
   desc "Query, filter, and clean your Mac Trash with ease"
   homepage "https://github.com/suryanox/scrap"
-  version "1.0.0"
+  version "1.0.1"
   license "MIT"
 
   on_macos do
     on_intel do
       url "https://github.com/suryanox/scrap/releases/download/v#{version}/scrap-x86_64-apple-darwin.tar.gz"
-      # sha256 "781af06249001695b970348006ee776f0058801a21bd9bc79d1b16b6513332f3"
+      # sha256 "43b0a285bd52b4fe0112b647627a58f81674d76b1a4e53b5eaf6b0b91ebdaa1f"
     end
 
     on_arm do
       url "https://github.com/suryanox/scrap/releases/download/v#{version}/scrap-aarch64-apple-darwin.tar.gz"
-      # sha256 "041e56d5f03adf798023077016d1bc16d1d388074ba4587325533f5a0bd713e6"
+      # sha256 "e97cc2938afb750083bccaa69b39e376b942158e0954188db297157ae37a1585"
     end
   end
 
